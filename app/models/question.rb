d863fe45efadef46af2cb887f2227744fdb4213f
syriusz <syriusz.p@gmail.com>
2014-07-20 09:46:37 +0200

    Added question title presence validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ActiveRecord::Base
+  validates_presence_of :title
+  
   belongs_to :user
   has_many :answers
+  
   
   def answer_accepted?
     (Answer.where(question_id: self.id).select {|answer| answer.accepted?}).empty? ? false : true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   # :recoverable, :rememberable and :trackable
   devise :database_authenticatable, :registerable, :validatable
 
+
   has_many :questions
   has_many :answers
 
