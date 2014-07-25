8a85d92f284066f4aa6c8782f429e1f8ad3ed1e6
syriusz <syriusz.p@gmail.com>
2014-07-25 10:56:04 +0200

    Added paperclip sucessfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:show]
 
   def show
   end
 
+  def create
+    @user = User.create( user_params )
+  end
+
   private
+
+	  # Use strong_parameters for attribute whitelisting
+	  # Be sure to update your create() and update() controller methods.
+
+	  def user_params
+	    params.require(:user).permit(:avatar)
+	  end
 
     def set_user
       @user = User.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722080747) do
+ActiveRecord::Schema.define(version: 20140725065646) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -43,12 +43,20 @@ ActiveRecord::Schema.define(version: 20140722080747) do
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "",  null: false
-    t.string   "encrypted_password", default: "",  null: false
+    t.string   "email",               default: "",  null: false
+    t.string   "encrypted_password",  default: "",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "points",             default: 100, null: false
+    t.integer  "points",              default: 100, null: false
     t.string   "username"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
