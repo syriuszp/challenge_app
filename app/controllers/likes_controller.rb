a9f21a0e205d487e674a75f8e7733fab2283efd1
syriusz <syriusz.p@gmail.com>
2014-07-29 19:45:50 +0200

    Added liking update by ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,25 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    
     if (Like.create(like_params)).valid?
       set_answer_author.earn_for_like
     end
-    redirect_to question_path set_question_id
+     
+     set_answer
+
+    respond_to do |format|
+      format.html { redirect_to question_path set_question_id and return}
+      format.js
+    end
+
   end
 
   private
+
+  def set_answer
+    @answer = Answer.find_by(id: params[:like][:answer_id])
+  end
 
   def set_answer_author
     User.find_by(id: Answer.find_by(id: params[:like][:answer_id]).user_id)
